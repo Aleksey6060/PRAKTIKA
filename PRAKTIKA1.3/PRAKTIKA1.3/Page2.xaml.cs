18f38434f8644d5352b34072bd4fea1640297fed
Aleksey6060 <145029148+Aleksey6060@users.noreply.github.com>
2024-03-16 00:01:32 +0300

    EntityFramework

---
+++ PRAKTIKA1.3/PRAKTIKA1.3/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRAKTIKA1._3
+{
+
+    public partial class Page2 : Page
+    {
+        private WatchStore111Entities context = new WatchStore111Entities();
+        public Page2()
+        {
+            InitializeComponent();
+            Orders.ItemsSource = context.Orders.ToList();
+        }
+    }
+}
---
+++ PRAKTIKA1.3/PRAKTIKA1.3/Page3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PRAKTIKA1._3
+{
+   
+    public partial class Page3 : Page
+    {
+        private WatchStore111Entities context = new WatchStore111Entities();
+        public Page3()
+        {
+            InitializeComponent();
+            Products.ItemsSource = context.Products.ToList();
+        }
+    }
+}
